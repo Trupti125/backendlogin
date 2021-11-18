4844325bfe866f51553f95965c15e9a752e245b4
Trupti Jadhawar <trupti_jadhawar@persistent.com>
2021-11-18 15:13:47 +0530

    first_login

---
+++ src/main/java/com/psl/miniproject21/model/User_status.java
@@ -0,0 +1,18 @@
+package com.psl.miniproject21.model;
+
+public class User_status {
+	private String status;
+
+	public User_status(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
